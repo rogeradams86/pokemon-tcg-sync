cf84a2512b4617c8f7cf214f18528c416a8d6aeb
rogeradams86 <166959485+rogeradams86@users.noreply.github.com>
2025-08-17 22:27:10 +0100

    Update fetch-pricing.js

--- scripts/fetch-pricing.js
+++ scripts/fetch-pricing.js
@@ -100,11 +100,11 @@ async function fetchPricingData() {
             keys.forEach(key => {
               if (key.trim()) {
                 pricingMap[key] = {
-  ...pricing,
-  productId,
-  groupName,
-  groupId
-};
+                  ...pricing,
+                  productId: productId,
+                  groupName: groupName,
+                  groupId: groupId
+                };
               }
             });
             
